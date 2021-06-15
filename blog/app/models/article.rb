2cc8cf98fa95e8238ac292a1d5f2b2398bf8f4ff
sz520qm <dudleylouw3@gmail.com>
2021-06-15 22:18:24 +0200

    add concerns to article model

--- blog/app/models/article.rb
+++ blog/app/models/article.rb
@@ -1,6 +1,11 @@
 class Article < ApplicationRecord
   has_many :comments
+  
+  VALID_STATUSES = ['public', 'private', 'archived']
 
-  validates :title, presence: true
-  validates :body, presence: true, length: { minimum: 10 }
+  validates :status, inclusion: { in: VALID_STATUSES }
+
+  def archived?
+    status == 'archived'
+  end
 end
