46450f2b0444b1ebf48ecd160776f4a9162b84d8
sz520qm <dudleylouw3@gmail.com>
2021-06-15 12:17:19 +0200

    add root route to routes file

--- blog/config/routes.rb
+++ blog/config/routes.rb
@@ -3,3 +3,9 @@ Rails.application.routes.draw do
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
+
+Rails.application.routes.draw do
+  root "articles#index"
+
+  get "/articles", to: "articles#index"
+end
