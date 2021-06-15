efb594c64ad4bd432a1bc4ecee948bc54d2f3585
sz520qm <dudleylouw3@gmail.com>
2021-06-15 21:02:49 +0200

    create  Comment model

--- blog/app/controllers/articles_controller.rb
+++ blog/app/controllers/articles_controller.rb
@@ -34,11 +34,11 @@ class ArticlesController < ApplicationController
       render :edit
     end
   end
-  
+
   def destroy
     @article = Article.find(params[:id])
     @article.destroy
-
+    
     redirect_to root_path
   end
 
