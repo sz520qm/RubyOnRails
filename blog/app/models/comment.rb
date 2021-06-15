addb763542fbd624cb0a1eb1875d5e56f38649c4
sz520qm <dudleylouw3@gmail.com>
2021-06-15 21:14:46 +0200

    update comments controller

--- blog/app/models/comment.rb
+++ blog/app/models/comment.rb
@@ -1,3 +1,12 @@
-class Comment < ApplicationRecord
-  belongs_to :article
+class CommentsController < ApplicationController
+  def create
+    @article = Article.find(params[:article_id])
+    @comment = @article.comments.create(comment_params)
+    redirect_to article_path(@article)
+  end
+
+  private
+    def comment_params
+      params.require(:comment).permit(:commenter, :body)
+    end
 end
